ea58b6e1f0d4025eb709f56c935102516f28bfa1
michael_austin <maxaustin92@gmail.com>
2017-11-28 04:51:44 -0500

    Initial commit

---
+++ app/src/main/java/com/maxaaustin/tuxedo/model/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.maxaaustin.tuxedo.model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.maxaaustin.tuxedo.R.layout.activity_login);
+    }
+}
