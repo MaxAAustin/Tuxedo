3d687f77939ef2ac2b1d6efd98790439f732f69a
michael_austin <maxaustin92@gmail.com>
2017-11-29 06:53:19 -0500

    Fixed Main, plus finished tab layout and ViewPager. App works, so far.

--- app/src/main/java/com/maxaaustin/tuxedo/model/MainActivity.java
+++ app/src/main/java/com/maxaaustin/tuxedo/model/MainActivity.java
@@ -6,8 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.maxaaustin.tuxedo.R;
+import com.maxaaustin.tuxedo.model.TweetFragment.OnFragmentInteractionListener;
 
-public class MainActivity extends AppCompatActivity /*implements MainActivityInterface*/{
+public class MainActivity extends AppCompatActivity implements HomeFragment.OnFragmentInteractionListener, OnFragmentInteractionListener, PreferencesFragment.OnFragmentInteractionListener /*implements MainActivityInterface*/{
 
     public static MainActivity instance;
     TabLayout mAllTabs;
@@ -22,17 +23,37 @@ public class MainActivity extends AppCompatActivity /*implements MainActivityInt
         setContentView(R.layout.activity_main);
         instance = this;
 
-        ViewPager mViewPager = (ViewPager) findViewById(R.id.tab_pager);
-        pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
-        pagerAdapter.add(new HomeFragment(), "Home");
-        pagerAdapter.add(new TweetFragment(), "Responses");
-        pagerAdapter.add(new PreferencesFragment(),"Lifestyle");
-
-        mViewPager.setAdapter(pagerAdapter);
+        //Creates a new TabLayout object and adds three tabs labelled with the following CharSequences.
         mAllTabs = (TabLayout) findViewById(R.id.main_tabs);
-        mAllTabs.setupWithViewPager(mViewPager);
+        mAllTabs.addTab(mAllTabs.newTab().setText("Home"));
+        mAllTabs.addTab(mAllTabs.newTab().setText("Tweets"));
+        mAllTabs.addTab(mAllTabs.newTab().setText("Lifestyle"));
 
+        mAllTabs.setTabGravity(android.support.design.widget.TabLayout.GRAVITY_FILL);
+        mAllTabs.setTabMode(android.support.design.widget.TabLayout.MODE_FIXED);
 
+        final ViewPager mViewPager = (ViewPager) findViewById(R.id.tab_pager);
+        final ViewPagerAdapter pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), mAllTabs.getTabCount());
+        mViewPager.setAdapter(pagerAdapter);
+
+        mViewPager.setOnPageChangeListener(new android.support.design.widget.TabLayout.TabLayoutOnPageChangeListener(mAllTabs));
+
+        mAllTabs.setOnTabSelectedListener(new android.support.design.widget.TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(android.support.design.widget.TabLayout.Tab tab) {
+                mViewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(android.support.design.widget.TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(android.support.design.widget.TabLayout.Tab tab) {
+
+            }
+        });
 
 //        String username = getIntent().getStringExtra("username");
 //        TextView mainUsername = (TextView) findViewById(R.id.main_username);
@@ -43,8 +64,10 @@ public class MainActivity extends AppCompatActivity /*implements MainActivityInt
         return instance;
     }
 
+    @Override
+    public void onFragmentInteraction(android.net.Uri uri) {
 
-
+    }
 
 
 //    @Override
--- app/src/main/java/com/maxaaustin/tuxedo/model/ViewPagerAdapter.java
+++ app/src/main/java/com/maxaaustin/tuxedo/model/ViewPagerAdapter.java
@@ -11,23 +11,38 @@ import java.util.List;
  * Created by michael on 11/29/2017.
  */
 
-class ViewPagerAdapter extends FragmentPagerAdapter {
+class ViewPagerAdapter extends android.support.v4.app.FragmentStatePagerAdapter {
+
+    int tabNum;
 
     private final List<Fragment> mFragmentList = new ArrayList<>();
     private final List<String> mFragNameList = new ArrayList<>();
 
-    public ViewPagerAdapter(FragmentManager manager) {
+    public ViewPagerAdapter(android.support.v4.app.FragmentManager manager, int tabNum) {
+
         super(manager);
+        this.tabNum = tabNum;
     }
 
     @Override
     public Fragment getItem(int position) {
-        return mFragmentList.get(position);
+        switch (position){
+            case 0: HomeFragment nHomeFrag = new HomeFragment();
+                    return nHomeFrag;
+            case 1: TweetFragment nTweetFrag = new TweetFragment();
+                    return nTweetFrag;
+            case 2: PreferencesFragment nPrefFrag = new PreferencesFragment();
+                    return  nPrefFrag;
+            default:
+                return null;
+        }
+
+//        return mFragmentList.get(position);
     }
 
     @Override
     public int getCount() {
-        return mFragmentList.size();
+        return tabNum;
     }
 
     public void add(Fragment mFrag, String title) {
